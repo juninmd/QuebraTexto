53aa245a89d691ed86e3e41cb86dcc7225f61d3d
Antonio <antonio@engsolutions.com.br>
2016-10-03 15:10:18 -0300

    Criando código

--- QuebraTexto/Program.cs
+++ QuebraTexto/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,62 @@ namespace QuebraTexto
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Local do Arquivo:");
+            string baseName = Console.ReadLine();
+            if (String.IsNullOrEmpty(baseName))
+                return;
+
+            Console.WriteLine("Quebrar a cada X Linhas:");
+            var quebrarEmXLinhas = Console.ReadLine();
+            if (String.IsNullOrEmpty(quebrarEmXLinhas))
+                return;
+
+            StreamWriter writer = null;
+            try
+            {
+                using (StreamReader inputfile = new System.IO.StreamReader(baseName))
+                {
+                    int count = 0;
+                    int countFiles = 0;
+                    string line;
+                    while ((line = inputfile.ReadLine()) != null)
+                    {
+
+                        if (writer == null || count > int.Parse(quebrarEmXLinhas))
+                        {
+                            ++countFiles;
+                            count = 0;
+                            if (writer != null)
+                            {
+                                writer.Close();
+                                writer = null;
+                            }
+
+                            if (!Directory.Exists(Path.GetDirectoryName(baseName) + "\\out\\"))
+                                Directory.CreateDirectory(Path.GetDirectoryName(baseName) + "\\out\\");
+
+                            writer =
+                                new System.IO.StreamWriter(Path.GetDirectoryName(baseName) + "\\out\\" +
+                                    Path.GetFileNameWithoutExtension(baseName) + countFiles + Path.GetExtension(baseName),
+                                    true);
+                        }
+
+                        writer.WriteLine(line.ToLower());
+
+                        ++count;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+
         }
     }
 }
